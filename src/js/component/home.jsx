80a513c59e17dcd949ca7b2c5197e3361e5e0d68
Vero2112 <verorodriguezruiz21@gmail.com>
2022-05-22 17:15:41 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Contador } from "./secondscounter.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid justify-content-center">
+			<div className="row">
+				<Contador />
+			</div>
 		</div>
 	);
 };
---
+++ src/js/component/secondscounter.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import propTypes from "prop-types";
+
+//create your first component
+export const Contador = (props) => {
+	return (
+		<>
+			<div className="col-12">
+				<div className="col-1 bg-dark text-white">
+					{props.digitoIcono}
+				</div>
+				<div className="col-1 bg-dark text-white">
+					{props.digitoUno % 10}
+				</div>
+				<div className="col-1 bg-dark text-white">
+					{props.digitoDos % 10}
+				</div>
+				<div className="col-1 bg-dark text-white">
+					{props.digitoTres % 10}
+				</div>
+				<div className="col-1 bg-dark text-white">
+					{props.digitoCuatro % 10}
+				</div>
+				<div className="col-1 bg-dark text-white">
+					{props.digitoCinco % 10}
+				</div>
+				<div className="col-1 bg-dark text-white">
+					{props.digitoSeis % 10}
+				</div>
+			</div>
+		</>
+	);
+};
+
+Contador.propTypes = {
+	digitoIcono: propTypes.string,
+	digitoUno: propTypes.number,
+	digitoDos: propTypes.number,
+	digitoTres: propTypes.number,
+	digitoCuatro: propTypes.number,
+	digitoCinco: propTypes.number,
+	digitoSeis: propTypes.number,
+};
+
+let counter = 0;
+setInterval(function () {
+	const one = Math.floor(counter / 100000);
+	const two = Math.floor(counter / 10000);
+	const three = Math.floor(counter / 1000);
+	const four = Math.floor(counter / 100);
+	const five = Math.floor(counter / 10);
+	const six = Math.floor(counter / 1);
+	counter++;
+
+	ReactDOM.render(
+		<Contador
+			digitoUno={one}
+			digitoDos={two}
+			digitoTres={three}
+			digitoCuatro={four}
+			digitoCinco={five}
+			digitoSeis={six}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
