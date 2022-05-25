3fde589d5d6617c430f6ba023d639677a914c953
Vero2112 <verorodriguezruiz21@gmail.com>
2022-05-25 20:03:13 +0000

    añadir boton restart y añadir condicional al countdown

--- src/js/component/secondscounter.jsx
+++ src/js/component/secondscounter.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import propTypes from "prop-types";
+import { func } from "prop-types";
 
 //create your first component
 export const Contador = (props) => {
@@ -16,9 +17,24 @@ export const Contador = (props) => {
 	const gettime = function () {
 		return counter;
 	};
+	// const countdown1 = function removeone() {
+	// 	counter--;
+	// 	setTimeout(removeone, 1000);
+	// };
+	const restarttimer = function restart() {
+		const one = Math.floor(counter / 100000);
+		const two = Math.floor(counter / 10000);
+		const three = Math.floor(counter / 1000);
+		const four = Math.floor(counter / 100);
+		const five = Math.floor(counter / 10);
+		const six = Math.floor(counter / 1);
+		counter++;
+		setTimeout(restart, 1000);
+	};
 
-	const countdown = function removeTime(time) {
+	const countdown = function removeTime() {
 		// clearInterval(myInterval);
+
 		const one = Math.floor(counter / 100000);
 		const two = Math.floor(counter / 10000);
 		const three = Math.floor(counter / 1000);
@@ -28,6 +44,7 @@ export const Contador = (props) => {
 		counter--;
 
 		setTimeout(removeTime, 1000);
+		if (six === 0 && five === 0) counter = 0;
 	};
 	// const myTimeout = setTimeout(removeTime, 1000);
 
@@ -71,6 +88,9 @@ export const Contador = (props) => {
 				<button onClick={gettime} className="m-2">
 					Resume
 				</button>
+				<button onClick={restarttimer} className="m-2">
+					Restart
+				</button>
 			</div>
 		</div>
 	);
@@ -87,7 +107,7 @@ Contador.propTypes = {
 };
 
 let counter = 0;
-const myInterval = setInterval(function generateTime(time) {
+const myInterval = setInterval(function generateTime() {
 	const one = Math.floor(counter / 100000);
 	const two = Math.floor(counter / 10000);
 	const three = Math.floor(counter / 1000);
